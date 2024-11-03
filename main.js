6fad86545c8f7f15eb75d1fb62c78d995b9ce67f
lina <ambowers@usf.edu>
2024-11-03 16:37:48 -0500

    Dynamically display tickets on the page

--- main.js
+++ main.js
@@ -20,4 +20,31 @@ async function fetchTickets (){
         }
         errorMessage.textContent='';
     }
-}
+
+// Task 3 display tickets dynamically on the page
+    tickets.forEach(ticket => {
+        const ticketDiv = document.createElement('div');
+        ticketDiv.className = 'ticket';
+
+        // display ticket id, customer name, issue description, and details
+        const ticketId = document.createElement('h3');
+        ticketId.textContent = `Ticket ID: ${ticket.id}`;
+        ticketId.appendChild(ticketId);
+
+        // use userId field as placeholder
+        const customerName = document.createElement('p');
+        customerName.textContent = `Customer Name: User ${ticket.userId}`;
+        ticketDiv.appendChild(customerName);
+
+        //use title field as placeholder
+        const issueDescription = document.createElement('p');
+        issueDescription.textContent = `Issue Description: ${ticket.title}`;
+
+        //use body field as placeholder
+        const details = document.createElement('p');
+        details.textContent = `Details: ${ticket.body}`;
+        ticketDiv.appendChild(details);
+
+        ticketContainer.appendChild(ticketDiv);
+    });
+} 
